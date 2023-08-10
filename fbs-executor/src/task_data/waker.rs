3a9b4710575c0049245e91bf33ce80c7d380422d
Marcin Gibuła <m.gibula@gmail.com>
2023-08-10 18:22:49 +0200

    Make miri happy

--- fbs-executor/src/task_data/waker.rs
+++ fbs-executor/src/task_data/waker.rs
@@ -6,36 +6,34 @@ use super::TaskData;
 use super::ExecutorCmd;
 
 fn create_vtable<T: 'static>() -> &'static RawWakerVTable {
-    unsafe {
-        &RawWakerVTable::new(
-            |s| waker_clone(&*(s as *const RefCell<TaskData<T>>)),
-            |s| waker_wake(&*(s as *const RefCell<TaskData<T>>)),
-            |s| waker_wake_by_ref(&*(s as *const RefCell<TaskData<T>>)),
-            |s| waker_drop(&*(s as *const RefCell<TaskData<T>>)),
-        )
-    }
+    &RawWakerVTable::new(
+        |s| waker_clone(s as *const RefCell<TaskData<T>>),
+        |s| waker_wake(s as *const RefCell<TaskData<T>>),
+        |s| waker_wake_by_ref(s as *const RefCell<TaskData<T>>),
+        |s| waker_drop(s as *const RefCell<TaskData<T>>),
+    )
 }
 
-fn waker_clone<T: 'static>(s: &RefCell<TaskData<T>>) -> RawWaker {
+fn waker_clone<T: 'static>(s: *const RefCell<TaskData<T>>) -> RawWaker {
     let waker_rc = unsafe { Rc::from_raw(s) };
     std::mem::forget(waker_rc.clone());
 
     RawWaker::new(Rc::into_raw(waker_rc) as *const (), create_vtable::<T>())
 }
 
-fn waker_wake<T: 'static>(s: &RefCell<TaskData<T>>) {
+fn waker_wake<T: 'static>(s: *const RefCell<TaskData<T>>) {
     let waker_rc = unsafe { Rc::from_raw(s) };
     waker_rc.borrow_mut().channel.send(ExecutorCmd::Schedule(waker_rc.clone()));
 }
 
-fn waker_wake_by_ref<T: 'static>(s: &RefCell<TaskData<T>>) {
+fn waker_wake_by_ref<T: 'static>(s: *const RefCell<TaskData<T>>) {
     let waker_rc = unsafe { Rc::from_raw(s) };
     std::mem::forget(waker_rc.clone());
 
     waker_rc.borrow_mut().channel.send(ExecutorCmd::Schedule(waker_rc.clone()));
 }
 
-fn waker_drop<T: 'static>(s: &RefCell<TaskData<T>>) {
+fn waker_drop<T: 'static>(s: *const RefCell<TaskData<T>>) {
     let rc = unsafe { Rc::from_raw(s) };
     drop(rc);
 }
