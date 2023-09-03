88d6980f546f209eb85feb4687290cf490611198
Marcin Gibuła <m.gibula@gmail.com>
2023-09-03 11:37:43 +0200

    Implement socket option setter

--- fbs-runtime/src/socket.rs
+++ fbs-runtime/src/socket.rs
@@ -1,5 +1,15 @@
-use std::os::fd::{OwnedFd, FromRawFd};
+use std::mem::size_of;
+use std::os::fd::{OwnedFd, FromRawFd, AsRawFd, RawFd};
+use std::io::Error;
+use super::socket_address::SocketIpAddress;
 use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum SocketError {
+    #[error("System error")]
+    SystemError(#[from] std::io::Error),
+}
+
 
 #[repr(i32)]
 pub enum SocketDomain {
@@ -65,8 +75,43 @@ impl Socket {
         }
     }
 
+    pub fn listen(&mut self, address: &SocketIpAddress, backlog: i32) -> Result<(), SocketError> {
+        let binary = address.to_binary();
+        unsafe {
+            let error = libc::bind(self.fd.as_raw_fd(), binary.sockaddr_ptr(), binary.length() as u32);
+            if error != 0 {
+                return Err(SocketError::SystemError(Error::last_os_error()));
+            }
+
+            let error = libc::listen(self.fd.as_raw_fd(), backlog);
+            if error != 0 {
+                return Err(SocketError::SystemError(Error::last_os_error()));
+            }
+
+            Ok(())
+        }
+    }
+
+    pub fn set_option(&mut self, option: SocketOptions) -> Result<(), SocketError> {
+        match option {
+            SocketOptions::ReuseAddr(value) => {
+                unsafe {
+                    let value: libc::c_int = value as libc::c_int;
+                    let error = libc::setsockopt(self.as_raw_fd(), libc::SOL_SOCKET, libc::SO_REUSEADDR, &value as *const i32 as *const libc::c_void, size_of::<libc::c_int>() as u32);
+                    if error != 0 {
+                        return Err(SocketError::SystemError(Error::last_os_error()));
+                    }
+                }
+            }
+        }
+
+        Ok(())
+    }
+
 }
 
-pub struct Listener {
-    sock: Socket,
+impl AsRawFd for Socket {
+    fn as_raw_fd(&self) -> RawFd {
+        self.fd.as_raw_fd()
+    }
 }
