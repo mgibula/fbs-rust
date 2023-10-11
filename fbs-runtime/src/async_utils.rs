c547e0f0079db70b69ac5ff56e04b5063522aeef
Marcin Gibuła <m.gibula@gmail.com>
2023-10-11 10:21:35 +0200

    Add AsyncSignal

--- fbs-runtime/src/async_utils.rs
+++ fbs-runtime/src/async_utils.rs
@@ -1,4 +1,5 @@
 use std::pin::Pin;
+use std::cell::Cell;
 use std::future::Future;
 use std::task::{Context, Waker, Poll};
 
@@ -113,6 +114,51 @@ pub fn async_channel_create<T>() -> (AsyncChannelRx<T>, AsyncChannelTx<T>) {
     )
 }
 
+struct AsyncSignalBackend {
+    fired: Cell<bool>,
+    waiters: Cell<Vec<Waker>>,
+}
+
+#[derive(Clone)]
+pub struct AsyncSignal {
+    ptr: Rc<AsyncSignalBackend>,
+}
+
+impl AsyncSignal {
+    pub fn new() -> Self {
+        Self { ptr: Rc::new(AsyncSignalBackend { fired: Cell::new(false), waiters: Cell::new(Vec::new()) }) }
+    }
+
+    pub fn signal(&self) {
+        self.ptr.fired.set(true);
+        self.ptr.waiters.take().into_iter().for_each(|w| w.wake());
+    }
+
+    pub fn is_signalled(&self) -> bool {
+        self.ptr.fired.get()
+    }
+
+    pub async fn wait(&self) {
+        self.clone().await;
+    }
+}
+
+impl Future for AsyncSignal {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        match self.ptr.fired.get() {
+            true => Poll::Ready(()),
+            false => {
+                let mut waiters = self.ptr.waiters.take();
+                waiters.push(cx.waker().clone());
+                self.ptr.waiters.set(waiters);
+
+                Poll::Pending
+            },
+        }
+    }
+}
+
 #[cfg(test)]
 mod test {
     use crate::{async_run, async_spawn};
@@ -136,6 +182,47 @@ mod test {
             });
 
             assert_eq!(result.await, 2);
+        });
+    }
+
+    #[test]
+    fn async_signal_test() {
+        async_run(async {
+            let (mut rx1, tx1) = async_channel_create::<i32>();
+            let tx2 = tx1.clone();
+            let sig1 = AsyncSignal::new();
+            let sig1cpy = sig1.clone();
+
+            let sig2 = AsyncSignal::new();
+            let sig2cpy = sig2.clone();
+
+            async_spawn(async move {
+                assert_eq!(sig1.is_signalled(), false);
+                sig1.wait().await;
+                assert_eq!(sig1.is_signalled(), true);
+
+                tx1.send(1);
+                sig2cpy.signal();
+            });
+
+            async_spawn(async move {
+                tx2.send(2);
+
+                assert_eq!(sig1cpy.is_signalled(), false);
+                sig1cpy.signal();
+                assert_eq!(sig1cpy.is_signalled(), true);
+
+                sig2.wait().await;
+                tx2.send(3);
+            });
+
+            let v1 = rx1.receive().await;
+            let v2 = rx1.receive().await;
+            let v3 = rx1.receive().await;
+
+            assert_eq!(v1, 2);
+            assert_eq!(v2, 1);
+            assert_eq!(v3, 3);
         });
     }
 }
